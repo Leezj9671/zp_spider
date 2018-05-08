7e4cfd4f602d77ed493f820296524a8deedc0520
lizhengjun <leezj9671@gmail.com>
2018-05-08 12:39:25 +0800

    UPDATE

---
+++ spider/config.py
@@ -0,0 +1,8 @@
+PROXY_API_URL = 'http://123.207.89.91:5000/get'
+
+SQL_ADDR = '127.0.0.1'
+SQL_PORT = 3306
+SQL_USER = 'root'
+SQL_PWD = 'toor'
+SQL_DATABASE = 'employ'
+SQL_TABLE = 'employ_info_test'
--- spider/lagou.py
+++ spider/lagou.py
@@ -4,21 +4,15 @@ import time
 import requests
 import pymysql.cursors
 
-from config import SQL_ADDR, SQL_DATABASE, SQL_PWD, SQL_TABLE, SQL_USER, PROXY_API_URL
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-    "DNT": "1",
-    "Host": "www.lagou.com",
-    "Origin": "https://www.lagou.com",
-    "Referer": "https://www.lagou.com/jobs/list_",  
-    "X-Anit-Forge-Code": "0",
-    "X-Anit-Forge-Token": None,
-    "X-Requested-With": "XMLHttpRequest" # 请求方式XHR
-}
+from config import SQL_ADDR, SQL_PORT, SQL_DATABASE, SQL_PWD, SQL_TABLE, SQL_USER, PROXY_API_URL
+from headers import get_lagou_header
 
 def save_json_to_mysql(jsondata):
+    """
+    保存json/dict数据到数据库中
+    """
     connection = pymysql.connect(host=SQL_ADDR,
+                             port=SQL_PORT,
                              user=SQL_USER,
                              password=SQL_PWD,
                              db=SQL_DATABASE,
@@ -26,23 +20,44 @@ def save_json_to_mysql(jsondata):
                              cursorclass=pymysql.cursors.DictCursor)
     try:
         with connection.cursor() as cursor:
-            sql = "INSERT INTO employ_info(city, companyFullName, companyShortName, companySize, createTime, district, education, financeStage, positionAdvantage, positionName, positionURL, salary, workYear, crawl_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())"
-            cursor.execute(sql, [jsondata["city"], jsondata["companyFullName"], jsondata["companyShortName"], jsondata["companySize"], jsondata["createTime"], jsondata["district"], jsondata["education"], jsondata["financeStage"], jsondata["positionAdvantage"], jsondata["positionName"], 'https://www.lagou.com/jobs/{}.html'.format(jsondata["positionId"]), jsondata["salary"], jsondata["workYear"],])
+            sql = "INSERT INTO {}(city, companyFullName, companyID,companyShortName, companySize, createTime, district, education, financeStage, positionAdvantage, positionName, positionID, positionURL, salary, workYear, crawl_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())".format(SQL_TABLE)
+            cursor.execute(sql, [jsondata["city"], jsondata["companyFullName"], jsondata["companyId"], jsondata["companyShortName"], jsondata["companySize"], jsondata["createTime"], jsondata["district"], jsondata["education"], jsondata["financeStage"], jsondata["positionAdvantage"].replace('&nbsp;', ''), jsondata["positionName"], jsondata["positionId"], 'https://www.lagou.com/jobs/{}.html'.format(jsondata["positionId"]), jsondata["salary"], jsondata["workYear"],])
         connection.commit()
+
+        # with connection.cursor() as cursor:
+        #     sql = "SELECT * FROM `{}`".format(SQL_TABLE)
+        #     cursor.execute(sql)
+        #     result = cursor.fetchone()
+        #     print(result)
     finally:
         connection.close()
 
 def getproxy():
-    return { "http": requests.get(PROXY_API_URL).text }
+    proxy = requests.get(PROXY_API_URL).text
+    return { "http": proxy, "https": proxy}
 
-ajax_url = 'https://www.lagou.com/jobs/positionAjax.json' 
+def main():
+    # 请求URL
+    ajax_url = 'https://www.lagou.com/jobs/positionAjax.json?px=new&needAddtionalResult=false&isSchoolJob=1'
+    # 抓取应届毕业生的招聘信息
+    post_param = {"first": "false", "pn": "1", "kd": ""}
+    for pn in range(1, 10):
+        post_param['pn'] = str(pn)
+        is_success = False
+        try_times = 10
+        result = ''
+        while not is_success:
+            print(pn)
+            try:
+                r = requests.post(ajax_url, headers=get_lagou_header(), data=post_param)
+                result = json.loads(r.text)
+                is_success = result['success']
+                for item in result["content"]["positionResult"]["result"]:
+                    save_json_to_mysql(item)
+            except:
+                time.sleep(1)
+                continue
+        time.sleep(5)
 
-post_param = {"first": "false", "pn": "1", "kd": ""} 
-
-for pn in range(1, 25):
-    post_param['pn'] = str(pn)
-    r = requests.post(ajax_url, headers=headers, data=post_param, proxies=getproxy())
-    time.sleep(2)
-    result = json.loads(r.text)
-    for item in result["content"]["positionResult"]["result"]:
-        save_json_to_mysql(item)
+if __name__ == '__main__':
+    main()
